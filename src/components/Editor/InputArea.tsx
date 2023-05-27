3d819c4dec17192f7ce5c4eaa46106dc32b91994
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2023-05-27 22:31:37 +0530

    feat: resize code editor screen

--- src/components/Editor/InputArea.tsx
+++ src/components/Editor/InputArea.tsx
@@ -1,13 +1,13 @@
 const InputArea = () => {
   return (
-    <div className="border-t-2 border-gray-600 h-16">
-      <div className="h-full w-max pt-4 pb-4 pl-6 pr-6 border-r-2 border-b-2 border-gray-600 text-lg">
+    <div className="border-t-2 border-gray-600 h-full">
+      <div className="h-16 w-max pt-4 pb-4 pl-6 pr-6 border-r-2 border-b-2 border-gray-600 text-lg">
         Input
       </div>
       <textarea
         name="input"
         id="input"
-        className="w-full h-full p-4 outline-none bg-transparent resize-none"
+        className="w-full h-[calc(100%-4rem)] p-4 outline-none bg-transparent resize-none"
       ></textarea>
     </div>
   );
